3ac62ab74dedd43f9171b577b7a181d7856d6f69
rneelams <ramesh.neelamsetti@capgemini.com>
2018-12-09 19:17:06 +0530

    updated model

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { EventService } from './shared/event.service';
 import { ActivatedRoute } from "@angular/router";
+import { IEvent } from "./shared/index";
 
 @Component({
    
@@ -18,7 +19,7 @@ import { ActivatedRoute } from "@angular/router";
     ` 
 })
 export class EventsListComponent implements OnInit {
-  events:any
+  events:IEvent
 
   constructor(private eventService: EventService, private route:ActivatedRoute) {
     
