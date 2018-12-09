0970897d56c8f5c08788ec98b706af5ce3d2df90
rneelams <ramesh.neelamsetti@capgemini.com>
2018-12-09 15:41:33 +0530

    added upto Routing in angular fundamentals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from "@angular/router";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { 
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver
+} from "./events/index";
+
+import { EventsAppComponent } from './events-app.component';
+import { NavBarCompoent } from './nav/navbar.component';
+import { Error404Component } from "./errors/404.component";
+
+import { appRoutes } from "./routes";
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavBarCompoent,
+    EventDetailsComponent,
+    CreateEventComponent,
+    Error404Component
+  ],
+  providers: [EventService, EventRouteActivator, EventListResolver,
+  { provide: 'canDeactivateCreateEvent', 
+    useValue: checkDirtyState
+  }
+  ],
+  bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
+
+export function checkDirtyState(component:CreateEventComponent) {
+  if (component.isDirty)
+  return window.confirm('You have not saved this event, do you really want to cancel?')
+  return true
+}
